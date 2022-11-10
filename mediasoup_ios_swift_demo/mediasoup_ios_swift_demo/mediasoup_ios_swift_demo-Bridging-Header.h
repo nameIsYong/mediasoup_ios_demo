6a4f3e58733c8eb20c21eb90a82101fb334c0dab
勇胡 <1228359686@qq.com>
2022-11-10 15:41:12 +0800

    11

--- mediasoup_ios_swift_demo/mediasoup_ios_swift_demo/mediasoup_ios_swift_demo-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#include "mediasoup_client_ios/Mediasoupclient.h"
-#import <mediasoup_client_ios/MediasoupDevice.h>
